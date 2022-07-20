f0fd7eaf94c83382d3f6949dbe6c9079e5f4427b
Alvaro Fernandez <afernandez@itba.edu.ar>
2022-07-20 20:27:15 -0300

    Agregamos los productos.

--- store/fakeProducts.js
+++ store/fakeProducts.js
@@ -1,28 +1,22 @@
 const fakeProducts = [
     {
       id: 1,
-      title: 'TV',
-      price: 600,
-      thumbnail: 'https://i.blogs.es/800731/captura-de-pantalla-2021-06-07-a-las-1.09.46/original.png'
+      title: 'Gulden Draak 330ml',
+      price: 640,
+      thumbnail: 'https://cdn.shopify.com/s/files/1/1103/5152/products/cerveza-gulden-draak_grande.jpg?v=1529635796'
     },
     {
       id: 2,
-      title: 'Laptop Asus',
-      price: 3000,
-      thumbnail: 'https://hiraoka.com.pe/media/catalog/product/cache/a357cb11a228eb6f7f15c0ee1ff203af/1/1/119950-1.jpg'
+      title: 'Leffe Bruin 330ml',
+      price: 580,
+      thumbnail: 'https://i0.wp.com/lacompra.com.do/wp-content/uploads/2021/10/5410228146162.jpg?fit=1000%2C1000&ssl=1'
     },
     {
       id: 3,
-      title: 'Teclado',
-      price: 300,
-      thumbnail: 'https://m.media-amazon.com/images/I/61DT+r681TL._AC_SY450_.jpg'
-    },
-    {
-      id: 4,
-      title: 'Peluche panda',
-      price: 100,
-      thumbnail: 'https://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2019/06/19154143/P146.jpg'
+      title: 'Leffe Blonde 330ml',
+      price: 580,
+      thumbnail: 'https://www.espaciovino.com.ar/media/default/0001/56/thumb_55987_default_medium.jpeg'
     }
   ]
   
-  module.exports = fakeProducts
+module.exports = fakeProducts
